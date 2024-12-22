0779682cf07e909b4ac5a6e5327b4e7b285d13c7
PabloHoties <pablohotiesmarques@gmail.com>
2024-12-22 12:10:21 -0300

    first commit

---
+++ src/main/java/br/com/cotiinformatica/dtos/HistoricoResponse.java
@@ -0,0 +1,17 @@
+package br.com.cotiinformatica.dtos;
+
+import java.util.Date;
+import java.util.UUID;
+
+import lombok.Data;
+
+@Data
+public class HistoricoResponse {
+
+	private UUID id;
+	private UUID funcionario_id;
+	private String operacao;
+	private String latitude;
+	private String longitude;
+	private Date dataHoraOperacao;
+}
